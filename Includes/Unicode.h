c483646debcdfb24544b7fce60fd22642cf7de7d
DarkFox <darkfox.tobias@outlook.com>
2026-08-29 12:37:42 +0200

    Add BlankTemplate 3gx.ld, types.h, Unicode, Helpers

---
+++ Includes/Unicode.h
@@ -0,0 +1,12 @@
+#ifndef UNICODE_H
+#define UNICODE_H
+
+// Unicode constants for CTRPluginFramework (from BlankTemplate)
+
+#define FONT_C 0x202A
+#define FONT_B 0x202B
+#define FONT_A 0x202C
+#define FONT_X 0x202D
+#define FONT_Y 0x202E
+
+#endif
